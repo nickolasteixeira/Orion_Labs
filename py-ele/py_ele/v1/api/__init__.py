c18c258d45ced5709731af5c944ed414bab7cab3
Eric Z. Eisberg <eeisberg@orionlabs.io>
2018-06-04 10:02:31 -0700

    Add method stubs

--- py-ele/py_ele/v1/api/__init__.py
+++ py-ele/py_ele/v1/api/__init__.py
@@ -15,14 +15,14 @@ class Vator(object):
     def __init__(self, floors, car_ct=1):
         self.floor_list = floors
 
-        first_floor = None
+        self.first_floor = None
         self.floor_map = {}
         for ix in range(len(floors)):
             unhashed_floor = ('floor-%s' % ix).encode('utf-8')
             fid = hashlib.sha1(unhashed_floor).hexdigest()
             self.floor_map[fid] = floors[ix]
-            if first_floor is None:
-                first_floor = fid
+            if self.first_floor is None:
+                self.first_floor = fid
 
         self.car_map = {}
         self.car_current_floor = {}
@@ -30,7 +30,7 @@ class Vator(object):
             name = ('Car-%s' % ix).encode('utf-8')
             cid = hashlib.sha1(name).hexdigest()
             self.car_map[cid] = name
-            self.car_current_floor[cid] = first_floor
+            self.car_current_floor[cid] = self.first_floor
 
     def floor_count(self):
         return len(self.floor_list)
@@ -46,6 +46,14 @@ class Vator(object):
     def current_floor(self, car_id):
         floor_id = self.car_current_floor[car_id]
         return {'id': floor_id, 'name': self.floor_map[floor_id]}
+
+    def find_closest_car(self, floor_id):
+        # To be completed
+        pass
+
+    def call_car(self, floor_id):
+        # To be completed
+        pass
 
 
 elevator = Vator(['B2', 'B1', 'MZ', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7'], 2)
